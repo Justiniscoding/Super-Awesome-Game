49656510a426b7d8145d182b889ca7348518d951
Dwait Pandhi <dwait.pandhi@gmail.com>
2026-02-28 11:36:37 -0500

    added a menu animation

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,7 +1,30 @@
 extends Node2D
 
-func _ready() -> void:
-	$play.connect("pressed", playGame)
+@onready var play_button = $play
+@onready var label = $play/Label
+@onready var bomb = $play/bomb
 
-func playGame() -> void:
+func _ready() -> void:
+	play_button.pressed.connect(_on_play_pressed)
+
+func _on_play_pressed() -> void:
+	play_button.disabled = true
+	label.text = ""
+
+	bomb.play("Fuse")
+	await bomb.animation_finished
+
+	await explode()
+
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
+
+
+func explode() -> void:
+	bomb.play("Explosion")
+	bomb.scale = Vector2.ONE
+
+	var target_scale = Vector2(10, 10)
+
+	var tween = create_tween()
+	tween.tween_property(bomb, "scale", target_scale, 0.6)
+	await tween.finished
