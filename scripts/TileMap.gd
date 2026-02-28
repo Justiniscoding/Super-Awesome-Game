9344d07bd1801a7be2eaff42f50b8d3de3c6e6ce
Dwait Pandhi <dwait.pandhi@gmail.com>
2026-02-28 12:40:28 -0500

    merge

--- scripts/TileMap.gd
+++ scripts/TileMap.gd
@@ -1,33 +1,41 @@
 extends TileMapLayer
 
-var blockMiningProgress = 0
-var blockPosition = Vector2i.ZERO
+var blockMiningProgress := 0.0
+var blockPosition: Vector2i = Vector2i.ZERO
 
-var miningTimes = {
+var miningTimes := {
 	Vector2i(0, 0): 0.5,
 	Vector2i(0, 1): 0.7
 }
 
-func mineBlock(playerPosition, timeMining):
-	# Get tile under player
-	var tilemapCellPosition = local_to_map(playerPosition + Vector2(0, 128))
+func mineBlock(playerPosition: Vector2, timeMining: float, dir: Vector2i) -> void:
+	# Use tile size (so we don't mine the "second block" by accident)
+	var tile_h: int = tile_set.tile_size.y
 
+	# A point around the player's feet (tweak 0.5 -> 0.4/0.6 if your pivot isn't centered)
+	var feet_cell: Vector2i = local_to_map(playerPosition + Vector2(0, tile_h * 0.1))
+
+	# Mine exactly 1 tile away in the chosen direction
+	var tilemapCellPosition: Vector2i = feet_cell + dir
+
+	# Reset progress when switching blocks
 	if tilemapCellPosition != blockPosition:
-		blockMiningProgress = 0
+		blockMiningProgress = 0.0
 		blockPosition = tilemapCellPosition
-	blockMiningProgress += timeMining
-	
-	var blockType = get_cell_atlas_coords(blockPosition)
 
+	blockMiningProgress += timeMining
+
+	var blockType: Vector2i = get_cell_atlas_coords(blockPosition)
 	if blockType == Vector2i(-1, -1):
 		return
 
+	# Crack / break using your atlas-coord logic
 	if blockType.x % 2 == 0:
-		if blockMiningProgress > miningTimes[blockType]:
-			blockMiningProgress = 0
+		if blockMiningProgress > miningTimes.get(blockType, 999999.0):
+			blockMiningProgress = 0.0
 			set_cell(tilemapCellPosition, 2, blockType + Vector2i(1, 0))
 	else:
-		if blockMiningProgress > miningTimes[blockType - Vector2i(1, 0)]:
-			blockMiningProgress = 0
+		var baseType := blockType - Vector2i(1, 0)
+		if blockMiningProgress > miningTimes.get(baseType, 999999.0):
+			blockMiningProgress = 0.0
 			erase_cell(tilemapCellPosition)
-	
--- scripts/player1.gd
+++ scripts/player1.gd
@@ -1,31 +1,36 @@
 extends CharacterBody2D
 
-@export var tilemap: PackedScene
+@export var speed: float = 200.0
+@export var gravity: float = 900.0
+@export var jump_force: float = -500.0
 
-@export var speed: float = 350.0
-@export var gravity: float = 2500.0
-@export var jump_force: float = -900.0
-
-var restartPosition = Vector2(250, 250)
+var mine_dir: Vector2i = Vector2i.DOWN
 
 func _physics_process(delta: float) -> void:
-	# gravity
 	if not is_on_floor():
 		velocity.y += gravity * delta
-	
-	# left and right
+
 	var input_dir := Input.get_axis("1move_left", "1move_right")
 	velocity.x = input_dir * speed
-	
-	# jumpy
+
 	if Input.is_action_just_pressed("1move_up") and is_on_floor():
 		velocity.y = jump_force
 
-	# mine
-	if is_on_floor() and Input.is_action_pressed("1move_down"):
-		get_parent().get_node("TileMapLayer").mineBlock(position, delta)
-		
-	if Global.playerDead1:
-		position = restartPosition
-		Global.playerDead1 = false
+	# Update direction (held is fine just to "aim")
+	if Input.is_action_pressed("1move_left"):
+		mine_dir = Vector2i.LEFT
+	elif Input.is_action_pressed("1move_right"):
+		mine_dir = Vector2i.RIGHT
+	elif Input.is_action_pressed("1move_up"):
+		mine_dir = Vector2i.UP
+	elif Input.is_action_pressed("1move_down"):
+		mine_dir = Vector2i.DOWN
+
+	# Spam-only: mine only on JUST PRESSED (one hit per press)
+	if Input.is_action_just_pressed("1move_left") \
+	or Input.is_action_just_pressed("1move_right") \
+	or Input.is_action_just_pressed("1move_up") \
+	or Input.is_action_just_pressed("1move_down"):
+		get_parent().get_node("TileMapLayer").mineBlock(global_position, 0.1, mine_dir)
+
 	move_and_slide()
