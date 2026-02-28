7884cdceee44630f94a7ae4da2d8cf0a0b2028b1
Justiniscoding <justinlasinski@gmail.com>
2026-02-28 18:18:30 -0500

    make timer become more and more red

--- scenes/gui.gd
+++ scenes/gui.gd
@@ -9,11 +9,17 @@ func updateScores() -> void:
 func _ready() -> void:
 	Global.connect("scoresChanged", updateScores)
 
+	var tween = get_tree().create_tween()
+	tween.tween_property($Timer, "modulate", Color(1.0, 0.0, 0.0), 181.0)
+	tween.play()
+
 func _process(delta) -> void:
 	timerTime -= delta
 
 	var minutes = floor(timerTime / 60)
-	var seconds = str(int(timerTime - (minutes * 60)))
+	var seconds =int(timerTime - (minutes * 60))
+
+	seconds = str(seconds)
 	if len(seconds) == 1:
 		seconds = "0" + seconds
 	$Timer.text = str(int(minutes)) + ":" +seconds
