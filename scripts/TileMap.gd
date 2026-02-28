675151311f335e2f11e023ea9affda4733c8f863
Justiniscoding <justinlasinski@gmail.com>
2026-02-28 12:02:44 -0500

    add new tilemap

--- scripts/TileMap.gd
+++ scripts/TileMap.gd
@@ -4,8 +4,8 @@ var blockMiningProgress = 0
 var blockPosition = Vector2i.ZERO
 
 var miningTimes = {
-	Vector2i(7, 4): 1,
-	Vector2i(7, 5): 1
+	Vector2i(0, 0): 0.5,
+	Vector2i(0, 1): 0.7
 }
 
 func mineBlock(playerPosition, timeMining):
@@ -22,6 +22,11 @@ func mineBlock(playerPosition, timeMining):
 	if blockType == Vector2i(-1, -1):
 		return
 
-	if blockMiningProgress > miningTimes[blockType]:
-		blockMiningProgress = 0
-		erase_cell(tilemapCellPosition)
+	if blockType.x % 2 == 0:
+		if blockMiningProgress > miningTimes[blockType]:
+			blockMiningProgress = 0
+			print(blockType)
+			print(blockType + Vector2i(1, 0))
+			set_cell(tilemapCellPosition, 0, blockType + Vector2i(1, 0))
+			# erase_cell(tilemapCellPosition)
+	
