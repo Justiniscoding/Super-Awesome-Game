d8a218b08272c0b7baa5522657e8ce214ec17686
Justiniscoding <justinlasinski@gmail.com>
2026-02-28 20:29:13 -0500

    fix win stuff

--- scenes/gui.gd
+++ scenes/gui.gd
@@ -2,7 +2,7 @@ extends Control
 
 signal timerOver
 
-var timerTime := 150.0
+var timerTime := 151.0
 var _done := false
 
 func updateScores() -> void:
@@ -25,8 +25,36 @@ func _process(delta: float) -> void:
 	if timerTime <= 0.0:
 		timerTime = 0.0
 		_done = true
-		emit_signal("timerOver")
+		get_parent().get_parent().get_node("player2").set_process(false)
+		get_parent().get_parent().get_node("player2").set_physics_process(false)
+		get_parent().get_parent().get_node("player").set_process(false)
+		get_parent().get_parent().get_node("player").set_physics_process(false)
 		set_process(false)
+
+		var tween = get_tree().create_tween()
+
+		var labelText = ""
+
+		tween.tween_property(get_parent().get_node("Game Over/bg2"), "position", Vector2(576,324), 1.0)
+		tween.tween_property(get_parent().get_node("Game Over/Label"), "position", Vector2(0, 0), 1.0)
+
+		if Global.p1points > Global.p2points:
+			labelText = "P1 Wins!"
+			tween.tween_property(get_parent().get_node("Game Over/Player 1"), "position", Vector2(1026, 250), 1.0)
+		elif Global.p2points > Global.p1points:
+			labelText = "P2 Wins!"
+			tween.tween_property(get_parent().get_node("Game Over/Player 2"), "position", Vector2(138, 250), 1.0)
+		else:
+			labelText = "It's a tie!"
+			tween.tween_property(get_parent().get_node("Game Over/Player 1"), "position", Vector2(1026, 250), 1.0)
+			tween.tween_property(get_parent().get_node("Game Over/Player 2"), "position", Vector2(138, 250), 1.0)
+
+		get_parent().get_node("Game Over/Label").text = labelText
+
+		tween.play()
+
+		get_tree().create_timer(8.0).connect("timeout", func():
+			get_tree().change_scene_to_file("res://scenes/Main.tscn"))
 		return
 
 	var minutes := int(timerTime / 60.0)
